5aa5d908df6c59307a8fb3ddf74b52f6d0483407
XanderJames1910 <xanderjames1910@gmail.com>
2021-03-20 06:13:16 -0500

    UI APP Design

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
+
+class HomePage extends StatelessWidget {
+  static final String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Preferencias de Usuario'),
+      ),
+      drawer: MenuWidget(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Color Secundario'),
+          Divider(),
+          Text('Genero:'),
+          Divider(),
+          Text('Nombre de usuario'),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/pages/settings_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:preferenciasusuarioapp/src/widgets/menu_widget.dart';
+
+class SettingsPage extends StatefulWidget {
+  static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nombre = 'Pedro';
+
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = new TextEditingController(text: _nombre);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Ajustes'),
+        ),
+        drawer: MenuWidget(),
+        body: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                'Settings',
+                style: TextStyle(
+                  fontSize: 45.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Divider(),
+            SwitchListTile(
+              value: _colorSecundario,
+              title: Text('Color secundario'),
+              onChanged: (value) {
+                _colorSecundario = value;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+              value: 1,
+              title: Text('Masculino'),
+              groupValue: _genero,
+              onChanged: (value) {
+                setState(() {
+                  _genero = value;
+                });
+              },
+            ),
+            RadioListTile(
+              value: 2,
+              title: Text('Femenino'),
+              groupValue: _genero,
+              onChanged: (value) {
+                setState(() {
+                  _genero = value;
+                });
+              },
+            ),
+            Divider(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                  labelText: 'Nombre',
+                  helperText: 'Nombre de la persona usando el teléfono',
+                ),
+                onChanged: (value) {},
+              ),
+            )
+          ],
+        ));
+  }
+}
